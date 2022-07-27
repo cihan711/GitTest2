9b3e88717aed90164d31851a1b7e1366b916c02e
cihan711 <mert.calculus716@gmail.com>
2022-07-27 23:04:28 +0300

    ilk projem

---
+++ src/gitAppium.java
@@ -0,0 +1,11 @@
+
+public class gitAppium {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Appium Gelistiriceleri"); 
+
+	}
+
+}
---
+++ src/gitSelenium.java
@@ -0,0 +1,11 @@
+
+public class gitSelenium {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Selenium Gelistiriceleri");    
+	   
+
+	}
+
+}
